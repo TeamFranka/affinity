361872f8ae4a8dd192d73d500bdc715faa8b8c8e
Benjamin Kampmann <ben@gnunicorn.org>
2020-11-28 16:41:20 +0100

    componentise

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,14 @@
 <template>
   <ion-app>
-    <ion-menu side="start" content-id="main-content">
-      <ion-header>
-        <ion-toolbar translucent>
-          <ion-title>Menu</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <ion-list>
-          <ion-item>
-            <ion-icon name="mail" slot="start"></ion-icon>
-            <ion-label>Inbox</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-icon name="paper-plane" slot="start"></ion-icon>
-            <ion-label>Outbox</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-icon name="heart" slot="start"></ion-icon>
-            <ion-label>Favorites</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-icon name="archive" slot="start"></ion-icon>
-            <ion-label>Archived</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-icon name="trash" slot="start"></ion-icon>
-            <ion-label>Trash</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-icon name="warning" slot="start"></ion-icon>
-            <ion-label>Spam</ion-label>
-          </ion-item>
-        </ion-list>
-      </ion-content>
-    </ion-menu>
-
-    <ion-popover
-      :is-open="isOpenRef"
-      css-class="my-custom-class"
-      :event="event"
-      :translucent="true"
-      @onDidDismiss="setOpen(false)"
-    >
-      <Popover></Popover>
-    </ion-popover>
-
+    <side-menu></side-menu>
     <div class="ion-page" id="main-content">
       <ion-header :translucent="true">
         <ion-toolbar>
             <ion-buttons slot="start">
               <ion-menu-button></ion-menu-button>
             </ion-buttons>
-          <ion-title slot="primary">{{result}}</ion-title>
             <ion-buttons slot="end">
-                <ion-avatar>
-                  <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y">
-                </ion-avatar>
+                <user-icon v-bind:user="user"></user-icon>
             </ion-buttons>
         </ion-toolbar>
       </ion-header>
@@ -67,12 +19,15 @@
 
 <script lang="ts">
 import {
-  IonApp, IonRouterOutlet, IonHeader, IonContent, IonLabel, IonItem, IonTitle, IonToolbar, IonIcon, IonButtons,
+  IonApp, IonRouterOutlet, IonHeader, IonContent, IonToolbar, IonIcon, IonButtons,
   IonList, IonMenu, IonMenuButton, IonAvatar,
   getPlatforms
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import Parse from 'parse';
+
+import SideMenu from './components/side-menu.vue';
+import UserIcon from './components/user-icon.vue';
 
 Parse.initialize("pJ63XHNU3C14XQpdLVYDbQR3mSU4aye4LQhxap3R", "73v5ZKTHd2Wxl71zJv7NiWXLbZlZXwnESZhCUtcH");
 Parse.serverURL = 'https://parseapi.back4app.com/';
@@ -86,26 +41,30 @@ export default defineComponent({
     IonApp,
     IonRouterOutlet,
     IonHeader,
-    IonMenu, IonList, IonLabel, IonItem, IonMenuButton,
-    IonAvatar,
-    IonContent,
-    IonTitle,
+    IonMenuButton,
+    UserIcon,
+    SideMenu,
     IonToolbar,
-    IonIcon,
     IonButtons,
   },
   data() {
     return {
       result: "loading",
-      user: null,
+      user: {},
       }
   },
   mounted() {
     Parse.User.currentAsync().then(user => {
       console.log('Logged user', user);
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = { avatar: "https://gnunicorn.org/assets/images/ben.png" };
+      }
     }, err => {
       console.log('Error getting logged user');
-    })
+      this.user = {avatar: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" };
+    });
 
     const install = new Parse.Installation();
     install.set("deviceType", getPlatforms().toString());
---
+++ src/components/user-icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-avatar>
+        <img v-bind:src="user.avatar" />
+    </ion-avatar>
+</template>
+
+
+<script lang="ts">
+import {
+  IonAvatar,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import Parse from 'parse';
+
+export default defineComponent({
+  name: 'UserIcon',
+  props: {
+      user: Object
+    },
+  components: {
+    IonAvatar
+  }
+});
+</script>
