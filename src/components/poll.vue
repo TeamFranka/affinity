65a6794eeac1eb0e0ea5f6cca328962c0954a743
Benjamin Kampmann <ben@gnunicorn.org>
2021-01-06 17:13:50 +0100

    poll closing UI fix

--- src/components/poll.vue
+++ src/components/poll.vue
@@ -48,18 +48,18 @@
       <ion-spinner v-if="loading" />
       <div class="ion-text-end" v-if="!loading && !showingResults">
         <ion-button
-          v-if="canShowResults && votedCount"
-          @click="wantsToShowResult = true"
-          fill="clear"
-          size="small"
-        >Zwischenergebnis zeigen</ion-button>
-        <ion-button
           v-if="!hasVoted"
           :disabled="!canSubmit"
           @click="submit"
           size="small"
           fill="outline"
         >Abstimmen</ion-button>
+        <ion-button
+          v-if="canShowResults"
+          @click="wantsToShowResult = true"
+          fill="clear"
+          size="small"
+        >Zwischenergebnis zeigen</ion-button>
       </div>
       <div class="ion-text-end" v-if="!loading && showingResults">
         <ion-button
@@ -281,7 +281,7 @@ export default defineComponent({
       return this.poll.get('options')
     },
     canShowResults(): boolean {
-      return this.isClosed || (this.poll.get('showResults') && this.poll.get("showsResultsWithoutVote"))
+      return !this.isClosed && (this.poll.get('showResults') && this.poll.get("showsResultsWithoutVote"))
     },
     canSubmit(): boolean {
       return !this.isClosed && this.selected.length > 0
