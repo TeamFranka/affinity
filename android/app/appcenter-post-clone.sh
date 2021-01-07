f23d0ea079af9c04492ec2df79319fa44f8a58a9
Benjamin Kampmann <ben@gnunicorn.org>
2021-01-07 19:35:37 +0100

    is this a better start script?

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
-echo "RUNNING POST CLONE -----------------------------------"
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo npm install yarn -g
-sudo npm install -g ionic
-cd ../../npm i
-ionic build
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+# # Required nodeJS version
+# NODE_VERSION=10.17.0
+
+# # workaround to override the v8 alias
+# npm config delete prefix
+# . ~/.bashrc
+# nvm install "$NODE_VERSION"
+# nvm alias node10 "$NODE_VERSION"
+
+# go to root of project
+cd ../..
+
+# install ionic globally
+npm install -g @ionic/cli
+
+# install dependencies
+npm i
+
+# run optimized production build
+npm run build -- --prod
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
 npx cap sync
