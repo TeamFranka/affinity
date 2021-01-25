8c85e70f896ebc4bb87d6cfee5347e480717eb91
Benjamin Kampmann <ben@gnunicorn.org>
2021-01-25 16:54:29 +0100

    link test

--- tests/e2e/specs/simple_post.js
+++ tests/e2e/specs/simple_post.js
@@ -15,4 +15,15 @@ describe('Posting to the feed as regular User', () => {
       cy.get('[data-cy-type=activity] [data-cy-role=content] p')
         .should('contain', testStr);
     })
+    it('Posts a Link', () => {
+      const testStr = `Hier cooler Link: https://twitter.com/franka4change !`;
+      cy.visit('/feed');
+      cy.get('[data-cy=newPost]').within(()=>{
+        cy.get('[data-cy=richEditor]').type(testStr);
+        cy.get('[data-cy-role=submit]').first().click();
+      })
+
+      cy.get('[data-cy-type=activity] [data-cy-role=link] a')
+        .should('href.contain', "https://twitter.com/franka4change");
+    })
   })
