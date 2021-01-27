b4f6c24970569e02b280f0238805fa4fcb960c16
Benjamin Kampmann <ben@gnunicorn.org>
2021-01-27 15:39:53 +0100

    fix link submission, fixes  #66

--- tests/e2e/specs/simple_post.js
+++ tests/e2e/specs/simple_post.js
@@ -4,7 +4,7 @@ describe('Posting to the feed as regular User', () => {
     beforeEach(() => {
       cy.loggedInAs("clara");
     })
-    it('Posts a simple message', () => {
+    it.skip('Posts a simple message', () => {
       const testStr = `Whatever, dude ${Math.abs(Math.random()*  1000000)}!`;
       cy.visit('/feed');
       cy.get('[data-cy=newPost]').within(()=>{
@@ -16,15 +16,23 @@ describe('Posting to the feed as regular User', () => {
         .should('contain', testStr);
     })
 
-    it.skip('Posts a Link', () => {
-      const testStr = `Hier cooler Link: https://twitter.com/franka4change !`;
+    it('Posts a Link', () => {
+      const testStr = `Hier cooler Link: https://twitter.com/bbcdoctorwho`;
       cy.visit('/feed');
       cy.get('[data-cy=newPost]').within(()=>{
         cy.get('[data-cy=richEditor]').type(testStr);
+        cy.get('[data-cy-type=link] a')
+          .should('have.attr', 'href')
+          .and('contain',"https://twitter.com/bbcdoctorwho")
+        cy.get('[data-cy-type=link]').within(()=>{
+          cy.get('ion-spinner').should("not.exist");
+          cy.get('input[name=title]').clear().type("Twitter: @BBCDoctorWho");
+        });
         cy.get('[data-cy-role=submit]').first().click();
       })
 
       cy.get('[data-cy-type=activity] [data-cy-role=link] a')
-        .should('href.contain', "https://twitter.com/franka4change");
+        .should('have.attr', 'href')
+        .and('contain',"https://twitter.com/bbcdoctorwho")
     })
   })
