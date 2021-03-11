441809e7058d9faba51b4dfb1c6210466817590d
Benjamin Kampmann <ben@gnunicorn.org>
2021-03-11 09:27:45 +0100

    full test

--- tests/e2e/specs/notifications-configuration.js
+++ tests/e2e/specs/notifications-configuration.js
@@ -4,23 +4,23 @@ describe('Notification Configuration', () => {
   it('anon mobile can disable notifications', () => {
 
     const testStr = `${Math.random().toString(36).substring(18)}!`;
+
+    const setup =  {isMobile: true, device: {
+        "deviceName": "iPhone 6",
+        "deviceModel": "as-asdf",
+        "appIdentifier": "jetzt.franka.affinity",
+        "appName": "TeamFranka",
+        "appVersion": "1.0",
+        "appBuild": "1",
+        "deviceType": "android",
+        "installationId": `adsfa${testStr}sdf23`,
+        "GCMSenderId": "11215447asdfasdfasd3685",
+        "deviceToken": `PbdmfVZt00nr3${testStr}70xa3nDXRRuI-JD`,
+    }};
     cy.viewport("iphone-6");
     cy.visit('/', {
         onBeforeLoad: (win) => {
-            console.log(win);
-            win.CYPRESS = { isMobile: true, device: {
-                    "deviceName": "iPhone 6",
-                    "deviceModel": "as-asdf",
-                    "appIdentifier": "jetzt.franka.affinity",
-                    "appName": "TeamFranka",
-                    "appVersion": "1.0",
-                    "appBuild": "1",
-                    "deviceType": "android",
-                    "installationId": `adsfa${testStr}sdf23`,
-                    "GCMSenderId": "11215447asdfasdfasd3685",
-                    "deviceToken": `PbdmfVZt00nr3${testStr}70xa3nDXRRuI-JD`,
-                }
-            };
+            win.CYPRESS = Object.assign({}, setup);
         }
     });
 
@@ -28,5 +28,24 @@ describe('Notification Configuration', () => {
     cy.get('[data-cy=anon-menu]').within(() => {
         cy.get('[data-cy=push-settings-link]').click();
     })
+
+    // Team News is enabled by default
+    cy.get('ion-item[data-cy-channel=news] ion-toggle').should("have.attr", "aria-checked", "true");
+    cy.get('ion-item[data-cy-channel=news] ion-toggle').click();
+    cy.get('ion-item[data-cy-channel=news] ion-toggle').should("have.attr", "aria-checked", "false");
+    cy.get('ion-item[data-cy-channel=notifications] ion-toggle').should("have.attr", "aria-checked", "false");
+    cy.get('ion-item[data-cy-channel=notifications] ion-toggle').click();
+    cy.get('ion-item[data-cy-channel=notifications] ion-toggle').should("have.attr", "aria-checked", "true");
+
+    /// give it some time
+    cy.wait(1000);
+    cy.reload({
+        onBeforeLoad: (win) => {
+            win.CYPRESS = Object.assign({}, setup);
+        }
+    });
+    // they are still like that.
+    cy.get('ion-item[data-cy-channel=news] ion-toggle').should("have.attr", "aria-checked", "false");
+    cy.get('ion-item[data-cy-channel=notifications] ion-toggle').should("have.attr", "aria-checked", "true");
   })
 })
