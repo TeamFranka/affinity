fc54ca4b8bab645939860b80d8e85ade03aa539f
Benjamin Kampmann <ben@gnunicorn.org>
2021-04-11 18:37:42 +0200

    other attempt

--- .gitpod/devserver.sh
+++ .gitpod/devserver.sh
@@ -3,7 +3,7 @@
 # Set timeout to the number of seconds you are willing to wait.
 timeout=500; counter=0
 
-CONTAINER=affinity_mongo_1
+CONTAINER=affinity_parse_1
 echo "Waiting for $CONTAINER to be ready (${counter}/${timeout})"
 
 until [[ $(docker inspect --format '{{json .State.Running}}' $CONTAINER 2> /dev/null) ]]; do
@@ -23,36 +23,3 @@ done
 echo "$CONTAINER running"
 echo "Containers running"
 docker-compose ps
-sleep 5s
-echo "Init db"
-
-# init db
-# export VUE_APP_PARSE_URL=$(gp url 8080)/parse
-# cat .env.development | sed 's,^VUE_APP_PARSE_URL=.*,VUE_APP_PARSE_URL=$(gp url 8080)/parse/,' > .env.development.gitpod
-
-npm run dev:db
-
-echo "mock data"
-# init db fixtures
-npm run dev:db:mock-data
-
-sleep 3s
-
-# create .env file for live server
-
-if [ ! -f .env.development.local.template ]; then
-  echo "ERROR: db init did not work"
-  exit 1
-fi
-
-# VUE_APP_PARSE_URL=$(gp url 8080)/parse
-
-echo "create env file"
-cat .env.development.local.template > .env.development.local
-cat >> .env.development.local << EOF
-
-VUE_APP_PARSE_URL="$(gp url 8080)/parse"
-EOF
-
-# run parse dev server
-(npm run dev:run-parse & npm run serve)
