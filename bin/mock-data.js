d6a0033f5e4359617634951216b328c88647a9bb
Benjamin Kampmann <ben@gnunicorn.org>
2021-04-11 21:03:47 +0200

    new targets

--- bin/mock-data.js
+++ bin/mock-data.js
@@ -3,6 +3,8 @@ require("./ts-cli-setup.js");
 const Parse = require('parse/node');
 const fs = require("fs");
 
+console.warn("Targeting", process.env.VUE_APP_PARSE_URL);
+
 Parse.initialize(
     process.env.VUE_APP_PARSE_APP_ID,
     process.env.VUE_APP_PARSE_JS_KEY,
