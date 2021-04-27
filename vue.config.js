8c96e18b2ac58c19738a55382cba33585532fb40
Benjamin Kampmann <ben@gnunicorn.org>
2021-04-27 16:52:31 +0200

    make sub

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
+const PAGE_DEFAULTS = {
+  entry: "src/main.ts",
+  favicon: "public/assets/icon/favicon.png",
+  template: "src/templates/app.html",
+  filename: "index.html",
+};
+
+
+
 module.exports = {
   pages: {
-    app: {
-      entry: "src/main.ts",
-      template: "src/templates/app.html",
-      filename: "index.html",
-    },
+    app: Object.assign({}, PAGE_DEFAULTS, {
+      templateParameters: {
+        title: "Affinity",
+        "AFFINITY_DEFAULT_TEAM": process.env.VUE_APP_DEFAULT_TEAM
+      }
+    }),
+    "wir-md": Object.assign({}, PAGE_DEFAULTS, {
+      filename: "wir.md.html",
+      templateParameters: {
+        title: "Wir.md",
+        "AFFINITY_DEFAULT_TEAM": "adbcdef",
+      }
+    }),
     widget: {
       entry: "src/chat-widget.js",
       template: "src/templates/chat-widget.html",
