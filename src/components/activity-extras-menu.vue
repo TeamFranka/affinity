3f145415e416cff4ac31efb85c379d942bd0601d
Benjamin Kampmann <ben@gnunicorn.org>
2021-05-02 17:45:57 +0200

    minor design improvements

--- src/components/activity-extras-menu.vue
+++ src/components/activity-extras-menu.vue
@@ -1,12 +1,11 @@
 <template>
   <ion-list>
     <ion-item v-if="isBookmarked" data-cy-role="unbookmark" button  @click="unbookmark">
-        <ion-icon :icon="bookmarkIcon" slot="start" />
+        <ion-icon color="success" :icon="bookmarkIcon" slot="start" />
         <i18n-t keypath="activity.extras.bookmarked" />
-        <ion-icon :icon="check" color="success" slot="end" />
     </ion-item>
     <ion-item v-else data-cy-role="bookmark" button  @click="bookmark">
-        <ion-icon :icon="bookmarkIcon" slot="start" />
+        <ion-icon color="dark" :icon="bookmarkIcon" slot="start" />
         <i18n-t keypath="activity.extras.bookmark" />
     </ion-item>
   </ion-list>
