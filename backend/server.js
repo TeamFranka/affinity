fd5b670c1911d793b2033e0d0c85230956b69b0b
Dave Gööck <dave@goeoeck.net>
2021-05-07 01:36:43 +0000

    use VUE_APP_PARSE_URL as publicServerUrl if present

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,24 @@ const app = express();
 global.app = app;
 global.Parse = Parse;
 
-const api = new ParseServer({
-    "liveQuery": {
-        "classNames": ["Conversation", "Message", "Activity"]
-    },
-    "appId": "APPLICATION_ID",
-    "masterKey": "MASTER_KEY",
-    "databaseURI": "mongodb://mongo/dev",
-    "cloud": "/parse-server/cloud/main.js",
-    "startLiveQueryServer": true
-});
+const config = {
+  "liveQuery": {
+    "classNames": ["Conversation", "Message", "Activity"]
+  },
+  "appId": "APPLICATION_ID",
+  "masterKey": "MASTER_KEY",
+  "databaseURI": "mongodb://mongo/dev",
+  "cloud": "/parse-server/cloud/main.js",
+  "startLiveQueryServer": true
+}
+if (process.env.VUE_APP_PARSE_URL) config.publicServerURL = process.env.VUE_APP_PARSE_URL;
+const api = new ParseServer(config);
 
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
 
-
 require("/parse-server/cloud/app.js");
 
-app.listen(1337, function() {
+app.listen(1337, function () {
   console.log('------- parse-server-example running on port 1337.');
 });
